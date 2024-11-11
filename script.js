a1a05fdae13bb145effbdcf7f39a1921f2839b89
samdo817 <phantom72008@gmail.com>
2024-11-12 06:21:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+// Menangani klik untuk membuka dan menutup dropdown
+document.querySelectorAll('.dropbtn').forEach(dropbtn => {
+    dropbtn.addEventListener('click', function(event) {
+        const dropdownContent = this.nextElementSibling;  // Mengambil elemen dropdown-content
+        const arrowIcon = this.querySelector('.arrow-icon');  // Mengambil ikon panah
+
+        // Toggle dropdown content visibility
+        if (dropdownContent.style.display === 'block') {
+            dropdownContent.style.display = 'none';  
+            arrowIcon.style.transform = 'rotate(0deg)';  
+        } else {
+            dropdownContent.style.display = 'block';  
+            arrowIcon.style.transform = 'rotate(180deg)'; 
+        }
+
+        event.stopPropagation();
+    });
+});
+
+
+document.addEventListener('click', function(event) {
+    if (!event.target.closest('.dropdown')) {
+
+        document.querySelectorAll('.dropdown-content').forEach(content => {
+            content.style.display = 'none';
+        });
+        
+        document.querySelectorAll('.arrow-icon').forEach(arrowIcon => {
+            arrowIcon.style.transform = 'rotate(0deg)';
+        });
+    }
+});
